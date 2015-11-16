5a348f0b255196d7571a1eb3755f43e845bc2e0c
rsbarhey <rsbarhey@zone18-wd.cs.ucalgary.ca>
2015-11-15 23:42:36 -0700

    Created CircularA and B classes, Server and Client classes

---
+++ CircularA.java
@@ -0,0 +1,13 @@
+
+public class CircularA {
+	private CircularB circularB;
+	
+	public CircularA()
+	{	
+	}
+	
+	public void SetCircle(CircularB circB)
+	{
+		this.circularB = circB;
+	}
+}
---
+++ CircularB.java
@@ -0,0 +1,13 @@
+
+public class CircularB {
+	private CircularA cricularA;
+	
+	public CircularB()
+	{
+	}
+	
+	public void SetCricle(CircularA circA)
+	{
+		this.cricularA = circA;
+	}
+}
