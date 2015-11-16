bc161916a09a348376eb5d7d4420ba2b69b03005
rsbarhey <rsbarhey@zone18-wd.cs.ucalgary.ca>
2015-11-16 00:54:41 -0700

    A simple interface to choose which object to create + a simple client implementation

--- Client.java
+++ Client.java
@@ -1,7 +1,56 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.Scanner;
 public class Client {
 	
 	public static void main(String[] args) {
-
+		try {
+			Socket socket = new Socket("localhost", 2225);
+			Scanner in = new Scanner(System.in);
+			PrintInstruction();
+			while(true)
+			{
+				String choice = in.nextLine();
+				if(choice.equalsIgnoreCase("quit"))
+				{
+					System.out.println("Quitting...");
+					break;
+				}
+				else
+				{
+					ProcessChoice(choice);
+				}
+			}
+			
+		} catch (UnknownHostException e) {
+			System.out.println("Failed to connect to server...");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Failed to connect to server...");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void PrintInstruction()
+	{
+		System.out.println("------------Object creator interface-----------------");
+		System.out.println("Please enter the number of the object you wish to create");
+		System.out.println("1- Primitive object");
+		
+		System.out.println("*****************************************************");
+		System.out.println("Type in Quit to quit the program");
+	}
+	public static void ProcessChoice(String choice)
+	{
+		switch(choice)
+		{
+		case "1":
+			System.out.println("You have chosen Primitive");
+			break;
+		default:
+			System.out.println("Unknown choice, please follow the instruction");
+			PrintInstruction();
+		}
 	}
 }
---
+++ Primitive.java
@@ -0,0 +1,13 @@
+
+public class Primitive {
+	private int intField;
+	private float floatField;
+	private String stringField;
+	
+	public Primitive(int intPara, float floatPara, String stringPara)
+	{
+		intField = intPara;
+		floatField = floatPara;
+		stringField = stringPara;
+	}
+}
